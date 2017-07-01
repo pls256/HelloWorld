18f72eb2244fcaffd4124a187f34f13b5de58b8b
pls256 <paul@256.com.au>
2017-07-01 15:19:11 +0800

    .

---
+++ goodbye.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 22 19:16:30 2016
+
+@author: Paul
+"""
+
+import random
+
+print(random.choice(['good','great']))
+print(random.choice(['bye','ta t']))
+
+
